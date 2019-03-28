cbcb3ef4c60d540fd153b3c54238861da63a15d5
lthnhung18i1 <48077411+lthnhung18i1@users.noreply.github.com>
2019-03-28 22:20:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("nhap danh sach cua sach");
+            n = Int32.Parse(Console.ReadLine());
+            Sachgiaokhoa[] mang = new Sachgiaokhoa[n];
+            for (int i = 1; i <= n; i++)
+            {
+                mang[i] = new Sachgiaokhoa();
+                mang[i].nhap();
+                mang[i].intt();
+            }
+            Sachgiaokhoa s1 = new Sachgiaokhoa();
+            s1.nhap();
+            s1.intt();
+            Console.ReadLine();
+        }
+    }
+}
